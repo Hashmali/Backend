fa26afc5964b0aee9cf3ba9f663c7605843f33e8
SgnvJaber <sgnvunleashed5@gmail.com>
2021-02-12 16:02:29 +0200

    fixed parser

--- api/info/views.py
+++ api/info/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.generics import RetrieveUpdateDestroyAPIView,RetrieveUpdateAPIView,RetrieveAPIView
 from .models import Info,Car
 from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import JSONParser
 
 class InfoUpdateView(RetrieveUpdateAPIView):
     def get_queryset(self):
@@ -10,19 +11,17 @@ class InfoUpdateView(RetrieveUpdateAPIView):
         return queryset
     serializer_class=InfoSerializers
     permission_classes=[IsAuthenticated,IsAdminUser]
-    parser_classes = [MultiPartParser]
+    parser_classes = [MultiPartParser,JSONParser]
+
 class InfoListView(RetrieveAPIView):
     def get_queryset(self):
         queryset = Info.objects.filter(id=self.kwargs["pk"])
         return queryset    
     serializer_class=InfoSerializers
     permission_classes=[IsAuthenticated,]
-
 class CarView(RetrieveUpdateDestroyAPIView):
     serializer_class=CarSerializer
     def get_queryset(self):
         queryset = Car.objects.filter(id=self.kwargs["pk"])
         return queryset
-
     permission_classes=[IsAuthenticated,]
-   
