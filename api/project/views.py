5acc8bea71cacccd432e84cafe7b047fe0ac31a6
SgnvJaber <sgnvunleashed5@gmail.com>
2021-02-21 18:44:35 +0200

    added parser to projects

--- api/project/views.py
+++ api/project/views.py
@@ -2,13 +2,15 @@ from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.generics import RetrieveUpdateDestroyAPIView,ListAPIView,CreateAPIView,ListCreateAPIView
 from .serializers import ProjectSerializer,FileSerializers
 from .models import Project,File
+from rest_framework.parsers import MultiPartParser,JSONParser
 
 # Create your views here.
 class ProjectView(ListAPIView):
     serializer_class=ProjectSerializer
     queryset=Project.objects.all()
    # permission_classes=(IsAuthenticated,)
-   
+    parser_classes = [MultiPartParser,JSONParser]
+
    
 
 class FileView(RetrieveUpdateDestroyAPIView):
@@ -27,6 +29,7 @@ class CreateProjectView(CreateAPIView):
     serializer_class=ProjectSerializer
     queryset=Project.objects.all()
    # permission_classes=(IsAuthenticated,IsAdminUser)
+    parser_classes = [MultiPartParser,JSONParser]
 
 class ManageProjectView(RetrieveUpdateDestroyAPIView):
     serializer_class=ProjectSerializer
@@ -34,3 +37,4 @@ class ManageProjectView(RetrieveUpdateDestroyAPIView):
         queryset = Project.objects.filter(id=self.kwargs["pk"])
         return queryset
     permission_classes=(IsAuthenticated,IsAdminUser)
+    parser_classes = [MultiPartParser,JSONParser]
